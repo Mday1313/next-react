97fd37f39a993f97883ea1f0fce3bc174cfe8975
Melissa Day <melissaday076@gmail.com>
2020-08-28 14:33:20 -0400

    basic set up for Next js with blog

--- pages/blog.js
+++ pages/blog.js
@@ -1,8 +1,21 @@
 import Layout from '../components/Layout';
+import Link from 'next/link';
+
+const PostLink = ({ slug, title }) => (
+    <li>
+        <Link as={`/${slug}`} href={`/post?title=${title}`}>
+            <a>{title}</a>
+        </Link>
+    </li>
+)
 
 const Blog = () => (
     <Layout title="My Blog">
-
+        <ul>
+           <PostLink slug="react-post" title="React Post" />
+           <PostLink slug="vue-post" title="Vue Post" />
+           <PostLink slug="angular-post" title="Angular Post" />
+        </ul>
     </Layout>
 )
 
---
+++ pages/post.js
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout';
+import { withRouter } from 'next/router';
+
+
+const Post = ({ router }) => (
+    <Layout title={router.query.title}>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium animi a alias asperiores sit! Aspernatur reprehenderit corporis repellat cumque ea, non modi quo. Nobis quod, a ad at fugiat voluptatibus.</p>
+    </Layout>
+)
+
+export default withRouter(Post);
